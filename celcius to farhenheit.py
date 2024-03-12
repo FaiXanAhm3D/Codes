f6a18c163636524fa2929b58f76f74cfd60b7b90
FaiXanAhm3D <faizanahmed2407000@gmail.com>
2024-03-12 12:02:02 +0530

    celcius to fahrenheit

---
+++ celcius to farhenheit.py
@@ -0,0 +1,6 @@
+#CelciustoFahrenheit
+
+Celsius=float(input("Enter temperature in degree celcius:"))
+print("Temperature in Celcius is:",Celsius)
+Fahrenheit=Celsius*9/5+32
+print("Temperature in Fahrenheit is:",Fahrenheit)
